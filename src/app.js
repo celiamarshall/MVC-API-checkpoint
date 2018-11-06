f45a1e3ea94be94461594cbcd7f52a8072ca3326
Celia Marshall <celia.m@gmail.com>
2018-11-06 10:45:49 -0800

    updated create and update functions in models

--- src/app.js
+++ src/app.js
@@ -10,12 +10,13 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 const routes = require('./routes/elephants')
 app.use('/elephants', routes)
 
-app.use((err, req, res, next) => {
-    res.status(err.status).json(err)
+app.use((req, res, next) => {
+    next({ status: 404, error: { message: 'Not found' } })
 })
 
-app.use((req, res, next) => {
-    res.status(404).json({ error: { message: 'Not found' } })
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(err.status || 500).json(err)
 })
 
 const listener = () => console.log(`Listening on port ${port}!`)
--- src/models/elephants.js
+++ src/models/elephants.js
@@ -5,6 +5,7 @@ function getAll(limit) {
     return limit ? elephants.slice(0, limit) : elephants
 }
 
+
 function getOne(id) {
     const errors = []
     let response
@@ -13,48 +14,69 @@ function getOne(id) {
 
     if (!elephantWithId) {
         errors.push('Elephant not found')
-        response = { errors }
-    }
-    else {
-        response = elephantWithId
+        return response = { errors }
     }
 
+    response = elephantWithId
     return response
 }
+
 
 function create({ species, habitat, endangered }) {
     const errors = []
     let response
 
-    if (!species) {
+    if (!species || species.length > 30) {
         errors.push('species is required')
+        return response = { errors }
     }
 
-    if (!habitat) {
+    if (!habitat || habitat.length > 30) {
         errors.push('habitat is required')
+        return response = { errors }
     }
 
     if (!endangered) {
         errors.push('endangered status is required')
+        return response = { errors }
     }
 
-    if (!species || !habitat || !endangered) {
-        response = { errors }
+    const newElephant = {
+        species,
+        habitat,
+        id: uuid()
     }
 
-    else {
-        const newElephant = {
-            species,
-            habitat,
-            endangered: !!endangered,
-            id: uuid()
-        }
-        elephants.push(newElephant)
-        response = newElephant
+    let boolean = endangeredStatus(endangered, newElephant)
+
+    if (boolean.error) {
+        errors.push(boolean.error)
+        return response = { errors }
     }
+
+    elephants.push(newElephant)
+    response = newElephant
 
     return response
 }
+
+
+function endangeredStatus (endangered, elephant) {
+    let response
+    let error = []
+    if (endangered === 'true') {
+        elephant.endangered = true
+    }
+    else if (endangered === 'false') {
+        elephant.endangered = false
+    }
+    else {
+        error.push('Please enter true or false')
+        return reponse = { error }
+    }
+    return elephant.endangered
+}
+
 
 function update(id, { species, habitat, endangered }) {
     const errors404 = []
@@ -73,10 +95,9 @@ function update(id, { species, habitat, endangered }) {
         return response = { errors400 }
     }
 
-    if (species) {
-        if (typeof species === 'string') {
+    if (species) { 
+        if (species.length <= 30) {
             elephantWithId.species = species
-            response = elephantWithId
         }
         else {
             errors400.push('Please enter a species name')
@@ -85,9 +106,8 @@ function update(id, { species, habitat, endangered }) {
     }
 
     if (habitat) {
-        if (typeof habitat === 'string') {
+        if (habitat.length <= 30) {
             elephantWithId.habitat = habitat
-            response = elephantWithId
         }
         else {
             errors400.push('Please enter a habitat name')
@@ -95,23 +115,20 @@ function update(id, { species, habitat, endangered }) {
         }
     }
 
+    let boolean
     if (endangered) {
-        if (endangered === 'true') {
-            elephantWithId.endangered = true
-            response = elephantWithId
-        }
-        else if (endangered === 'false') {
-            elephantWithId.endangered = false
-            response = elephantWithId
-        }
-        else {
-            errors400.push('Please enter true or false')
-            return reponse = { errors400 }
+        boolean = endangeredStatus(endangered, elephantWithId)
+        if (boolean.error) {
+            errors400.push(boolean.error)
+            return response = { errors400 }
         }
     }
 
+
+    response = elephantWithId
     return response
 }
+
 
 function remove(id) {
     const errors = []
@@ -125,9 +142,7 @@ function remove(id) {
     }
 
     response = elephantWithId
-
     elephantIdx = elephants.indexOf(elephantWithId)
-
     elephants.splice(elephantIdx, 1)
 
     return response
